32009251abf7f8ff6412a0db1d1eae744db4bdb8
Emil Abraham <emilabraham1414@gmail.com>
2015-02-13 11:04:41 -0500

    Refactored lesson 9 to allow any number of arguments.

--- juggling-async-lesson-9.js
+++ juggling-async-lesson-9.js
@@ -1,49 +1,43 @@
 var http = require('http');
 var bl = require('bl');
 
-var array = ['first', 'second', 'third'];
+var array = [];
 var finished = 0;
+var num_of_arguments = process.argv.length;
 
-// First GET
-http.get(process.argv[2], function (response) {
-  response.pipe(bl(function (err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    array[0] = data.toString();
-    finished++;
-    print(finished);
-  }));
-});
+// Initialize the array
+for (var z = 0; z < num_of_arguments-2; z++) {
+  array[z] = 'initialized';
+}
 
-// Second GET
-http.get(process.argv[3], function (response) {
-  response.pipe(bl(function (err, data) {
-    if (err) {
-     return console.error(err);
-    }
-    array[1] = data.toString();
-    finished++;
-    print(finished);
-  }));
-});
+// queue the requests
+var queue_requests = function () {
+  for (var x = 2; x < num_of_arguments; x++) {
+    httpcall(x);
+  }
+};
 
-// Third GET
-http.get(process.argv[4], function (response) {
-  response.pipe(bl(function (err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    array[2] = data.toString();
-    finished++;
-    print(finished);
-  }));
-});
-
+// pass in the index of URL argument to be processed.
+var httpcall = function (index) {
+  http.get(process.argv[index], function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err) {
+        return console.error(err);
+      }
+      // Story in array at proper position based on argument index
+      array[index-2] = data.toString();
+      finished++;
+      print(finished);
+    }));
+  });
+}
+// Print out the array if all requests are finished.
 var print = function(finished) {
-  if (finished === 3) {
+  if (finished === num_of_arguments - 2) {
     for (var i = 0; i < array.length; i++) {
       console.log(array[i]);
     }
   }
 }
+
+queue_requests();
