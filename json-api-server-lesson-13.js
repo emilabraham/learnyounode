7b216e2ceb817afca91256ecd9eeaf8cac7ab591
Emil Abraham <emilabraham1414@gmail.com>
2015-02-14 00:31:30 -0500

    Running into some errors with indentation and unexpected end of input

--- json-api-server-lesson-13.js
+++ json-api-server-lesson-13.js
@@ -6,6 +6,19 @@ var portnumber = process.argv[2];
 var server = http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'application/json' });
   var parsedurl = url.parse(request.url, true);
-  console.log(JSON.stringify(parsedurl));
+  var date = new Date();
+  if (0 === 0) {
+    hour = date.getHours();
+    minute = date.getMinutes();
+    second = date.getSeconds();
+    var parsedtime = 'hour:'+hour+',minutes:'+minute+',second:'+second+' ';
+    response.write(JSON.stringify(parsedtime));
+    response.end();
+  }
+  /*if (parsedurl.pathname  === '/api/parsetime') {
+    var parsedtime = '{hour: ' + date.getHours() + ', minute: ' +
+    date.getMinutes() + ', second: ' + date.getSeconds() + '}';
+    response.write(JSON.stringify(parsedtime));
+    }*/
 });
 server.listen(portnumber);
