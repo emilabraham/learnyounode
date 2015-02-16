6e0b6aa50d430ba4250db42aec64a9156de5f250
Emil Abraham <emilabraham1414@gmail.com>
2015-02-16 09:49:02 -0500

    Got a solution. Need to refactor.

--- json-api-server-lesson-13.js
+++ json-api-server-lesson-13.js
@@ -6,19 +6,21 @@ var portnumber = process.argv[2];
 var server = http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'application/json' });
   var parsedurl = url.parse(request.url, true);
-  var date = new Date();
-  if (0 === 0) {
-    hour = date.getHours();
-    minute = date.getMinutes();
-    second = date.getSeconds();
-    var parsedtime = 'hour:'+hour+',minutes:'+minute+',second:'+second+' ';
-    response.write(JSON.stringify(parsedtime));
+  if (parsedurl.pathname === '/api/parsetime') {
+  var date = new Date(parsedurl.query.iso);
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+  var parsedtime = { 'hour': hour, 'minute': minute, 'second': second };
+  response.end(JSON.stringify(parsedtime));
+  }
+  else if (parsedurl.pathname == '/api/unixtime') {
+    var date = new Date(parsedurl.query.iso);
+    var parsedtime = { 'unixtime': date.getTime() };
+    response.end(JSON.stringify(parsedtime));
+  }
+  else {
     response.end();
   }
-  /*if (parsedurl.pathname  === '/api/parsetime') {
-    var parsedtime = '{hour: ' + date.getHours() + ', minute: ' +
-    date.getMinutes() + ', second: ' + date.getSeconds() + '}';
-    response.write(JSON.stringify(parsedtime));
-    }*/
 });
 server.listen(portnumber);
